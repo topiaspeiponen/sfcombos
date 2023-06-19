dbb4ee4d37ea5c89436c8706c818231125169193
Topias Peiponen <topias.peiponen@gmail.com>
2023-06-19 16:28:02 +0300

    fix: change semantic release config

--- release.config.js
+++ release.config.js
@@ -5,6 +5,18 @@ const config = {
       '@semantic-release/commit-analyzer',
       {
         "preset": "conventionalcommits",
+        presetConfig: {
+          "types": [
+            {"type": "feat", "section": "Features"},
+            {"type": "fix", "section": "Bug Fixes"},
+            {"type": "chore", "hidden": true},
+            {"type": "docs", "section": "Changes"},
+            {"type": "style", "section": "Changes"},
+            {"type": "refactor", "section": "Changes"},
+            {"type": "perf", "section": "Changes"},
+            {"type": "test", "hidden": true}
+          ]
+        }
       }
     ],
     '@semantic-release/release-notes-generator',
